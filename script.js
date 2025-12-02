f53089fa85768dc20953f4cb145e410811ee5e3b
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2025-12-02 15:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
   // write your code here
+	let res="";
+	res+=text.toLowerCase();
+	res+='-';
+	res+=text.toUpperCase();
+	return res;
 }
 
 // DO not change the code below
